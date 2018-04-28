943239a4d2039595f60fe764995839ec6b8da493
marcus delbianco <mdelbianco9@gmail.com>
2018-04-27 23:39:28 -0700

    Added Fizz/Buzz condition

--- js/main.js
+++ js/main.js
@@ -107,6 +107,22 @@ function computeGrades() {
 console.log(computeGrades());
 
 
+// Adds words for numbers if condition is met
+
+function fizbuz() {
+  for(i=0; i<=100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+    console.log(i+"FizzBuzz");
+    }else if(i % 3 === 0) {
+      console.log('Fizz');
+    }else if(i % 5 === 0) {
+      console.log("Buzz");
+    }else{
+    console.log(i);
+    }
+  
+  }
+};
 
 
 
