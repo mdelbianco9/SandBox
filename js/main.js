ffbc05ef9ee4001f4de58a4c22f98e1b79ac8a1d
marcus delbianco <mdelbianco9@gmail.com>
2018-05-06 22:30:05 -0700

    creditcard function

--- js/main.js
+++ js/main.js
@@ -150,4 +150,34 @@ function armNum(a) {
 
 armNum(371);
 
+// Checks if number is a credit card number
+function credCheck(n) {
+var num = n;
+var arry = num.toString().split('')
+  if(arry.length == 16) {
+  return true
+  }else{
+  return false
+  console.log(arry.length);
+  }
+}
 
+console.log(credCheck(3627272890897652));
+
+function is_creditCard(str)
+{
+ regexp = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/;
+  
+        if (regexp.test(str))
+          {
+            return true;
+          }
+        else
+          {
+            return false;
+          }
+}
+
+console.log(is_creditCard("378282246310006"));
+
+console.log(is_creditCard("37828224630006"));
