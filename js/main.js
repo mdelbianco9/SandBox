bf69db923436937d0987a46adc82c6517a7d966d
marcus delbianco <mdelbianco9@gmail.com>
2018-05-08 00:01:01 -0700

    email validation

--- js/main.js
+++ js/main.js
@@ -198,3 +198,19 @@ function upper_case(str)
 }
 upper_case('Abcd');
 upper_case('abcd');
+
+// Email Validation
+function valid_email(str)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(mailformat.test(str))
+{  
+console.log("Valid email address!");  
+}  
+else  
+{  
+console.log("You have entered an invalid email address!");  
+}
+}
+
+valid_email('me-info@example.com');
