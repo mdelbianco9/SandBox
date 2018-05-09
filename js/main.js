3fc915efaa2b692ed1ca2f27e4010ff41cf1d454
marcus delbianco <mdelbianco9@gmail.com>
2018-05-08 21:02:29 -0700

    Word counter

--- js/main.js
+++ js/main.js
@@ -214,3 +214,23 @@ console.log("You have entered an invalid email address!");
 }
 
 valid_email('me-info@example.com');
+
+// Counts number of words in a string
+function getNum(str){
+  let splt = str.split(' ');
+  console.log(splt.length);
+};
+
+getNum("hello this is a word")
+
+function count_words()
+{
+str1= document.getElementById("InputText").value;
+//exclude  start and end white-space
+str1 = str1.replace(/(^\s*)|(\s*$)/gi,"");
+//convert 2 or more spaces to 1  
+str1 = str1.replace(/[ ]{2,}/gi," ");
+// exclude newline with a start spacing  
+str1 = str1.replace(/\n /,"\n");
+document.getElementById("noofwords").value = str1.split(' ').length;
+}
