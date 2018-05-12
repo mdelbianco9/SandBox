c5047603dbabaea68b2431236e5b82ef814c0abf
marcus delbianco <mdelbianco9@gmail.com>
2018-05-12 12:03:30 -0700

    Get property from object

--- js/main.js
+++ js/main.js
@@ -234,3 +234,33 @@ str1 = str1.replace(/[ ]{2,}/gi," ");
 str1 = str1.replace(/\n /,"\n");
 document.getElementById("noofwords").value = str1.split(' ').length;
 }
+
+
+// Get Property from an object
+function _keys(obj) 
+ {
+    if (!isObject(obj)) return [];
+    if (Object.keys) return Object.keys(obj);
+    var keys = [];
+    for (var key in obj) if (_.has(obj, key)) keys.push(key);
+    return keys;
+  }
+function isObject(obj) 
+{
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  }
+console.log(_keys({red: "#FF0000", green: "#00FF00", white: "#FFFFFF"}));
+
+
+
+
+
+
+
+
+
+
+
+
+
